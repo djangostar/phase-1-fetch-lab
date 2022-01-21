219dfe2ca63550d24a01cb89e452e7abf899b47c
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-01-20 19:26:41 -0800

    commit tree 64a65c231f5ee18e85d2a2cb938b89fd8e167ca7

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+// To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((res) => res.json())
+  .then((books) => renderBooks(books))
 }
 
 function renderBooks(books) {
+  console.log(books[4])
+  
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  //document.body.append(totalPages(books))
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+let total = 0
+
+function totalPages(books) {
+  for(let book of books) {
+    total += book.numberOfPages
+  }
+  return total
+}
+
